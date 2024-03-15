b63b155e81ecda7178b0c0b5fc4787a65cf1ad4f
never47 <guseinovi2002@gmail.com>
2024-03-15 10:24:23 +0100

    add: meal_item

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -4,15 +4,17 @@ import 'package:meals_app/models/category.dart';
 class CategoryGridItem extends StatelessWidget {
   const CategoryGridItem({
     required this.category,
+    required this.onSelectCategory,
     super.key,
   });
 
   final Category category;
+  final void Function() onSelectCategory;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {},
+      onTap: onSelectCategory,
       splashColor: Theme.of(context).primaryColor,
       borderRadius: BorderRadius.circular(16),
       child: Container(
