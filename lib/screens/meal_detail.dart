b99fe5f59aa25a94a16febc9d1dcb860620096c9
never47 <guseinovi2002@gmail.com>
2024-03-18 11:59:04 +0100

    add: favorites provider | improved fav logic

--- lib/screens/meal_detail.dart
+++ lib/screens/meal_detail.dart
@@ -1,25 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals_app/providers/favorites_provider.dart';
 
-class MealDetail extends StatelessWidget {
+class MealDetail extends ConsumerWidget {
   const MealDetail({
     super.key,
-    required this.onToggleFavotie,
     required this.meal,
   });
 
   final Meal meal;
-  final void Function(Meal meal) onToggleFavotie;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
         actions: [
           IconButton(
             onPressed: () {
-              onToggleFavotie(meal);
+              final wasAdded = ref
+                  .read(favoriteMealsProvider.notifier)
+                  .toggleMealFavoriteStatus(meal);
+              ScaffoldMessenger.of(context).clearSnackBars();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(
+                      wasAdded ? 'Meal added as a favorite.' : 'Meal removed.'),
+                ),
+              );
             },
             icon: const Icon(Icons.star),
           ),
